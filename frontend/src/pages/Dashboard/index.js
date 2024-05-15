de2a97d9237323a7cb2406cabdb821ac6e41527b
atendechat <160610153+atendechat@users.noreply.github.com>
2024-05-15 19:04:47 -0300

    Update index.js

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -426,7 +426,7 @@ const Dashboard = () => {
                   <CallIcon
                     style={{
                       fontSize: 100,
-                      color: "#0b708c",
+                      color: "#FFFFFF",
                     }}
                   />
                 </Grid>
@@ -463,7 +463,7 @@ const Dashboard = () => {
                   <HourglassEmptyIcon
                     style={{
                       fontSize: 100,
-                      color: "#47606e",
+                      color: "#FFFFFF",
                     }}
                   />
                 </Grid>
@@ -542,7 +542,7 @@ const Dashboard = () => {
                   <CheckCircleIcon
                     style={{
                       fontSize: 100,
-                      color: "#5852ab",
+                      color: "#FFFFFF",
                     }}
                   />
                 </Grid>
@@ -579,7 +579,7 @@ const Dashboard = () => {
                   <GroupAddIcon
                     style={{
                       fontSize: 100,
-                      color: "#8c6b19",
+                      color: "#FFFFFF",
                     }}
                   />
                 </Grid>
@@ -617,7 +617,7 @@ const Dashboard = () => {
                   <AccessAlarmIcon
                     style={{
                       fontSize: 100,
-                      color: "#7a3f26",
+                      color: "#FFFFFF",
                     }}
                   />
                 </Grid>
@@ -654,7 +654,7 @@ const Dashboard = () => {
                   <TimerIcon
                     style={{
                       fontSize: 100,
-                      color: "#8a2c40",
+                      color: "#FFFFFF",
                     }}
                   />
                 </Grid>
