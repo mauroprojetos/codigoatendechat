b4c059ba0367b04d27aa9f0e8036ce4d8840279e
atendechat <160610153+atendechat@users.noreply.github.com>
2024-06-19 18:20:13 -0300

    Update MainListItems.js

--- frontend/src/layout/MainListItems.js
+++ frontend/src/layout/MainListItems.js
@@ -506,7 +506,7 @@ const MainListItems = (props) => {
               </Hidden> 
               */}
               <Typography style={{ fontSize: "12px", padding: "10px", textAlign: "right", fontWeight: "bold" }}>
-                {`${versionSystem}`}
+                {`${version}`}
 
               </Typography>
             </React.Fragment>
